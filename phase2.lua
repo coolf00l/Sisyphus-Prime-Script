0a8ab96db32517a2dbe3b0ed1efcada82ba898c4
SU01 <aezelt1@gmail.com>
2026-05-13 21:37:11 +0200

    Create phase2.lua

---
+++ phase2.lua
@@ -0,0 +1,22 @@
+-- phase2.lua
+local module = {}
+
+function module.init(player, character, Config, VoiceLines, Dash, Punch, Shockwave)
+    local humanoid = character:WaitForChild("Humanoid")
+    local phase2 = false
+
+    humanoid.HealthChanged:Connect(function(newHealth)
+        if phase2 then return end
+        if newHealth <= humanoid.MaxHealth * Config.Phase2.TriggerHealthPercent then
+            phase2 = true
+            VoiceLines.onPhase2()
+
+            humanoid.WalkSpeed = humanoid.WalkSpeed * Config.Phase2.WalkSpeedMultiplier
+            Dash.setPhase2(Config.Phase2.DashSpeedMultiplier)
+            Punch.setPhase2(Config.Phase2.PunchDamageMultiplier)
+            Shockwave.setPhase2(1.2)
+        end
+    end)
+end
+
+return module
